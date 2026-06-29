bd80beb31b80d8802a82eb0a58bbc12c2921f2c2
Lxk_buaa <867768510@qq.com>
2026-06-29 23:32:22 +0800

    Bump Gazebo sim tools package version

--- .xgc2/scripts/check_package_compliance.sh
+++ .xgc2/scripts/check_package_compliance.sh
@@ -71,9 +71,9 @@ if rg -n --glob '!.xgc2/scripts/check_package_compliance.sh' \
   exit 1
 fi
 
-grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.0-1)" .xgc2/product.yml
+grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.1-1)" .xgc2/product.yml
 grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.0-1)" .xgc2/product.yml
-grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.0-1)" .xgc2/scripts/package_debs.sh
+grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.1-1)" .xgc2/scripts/package_debs.sh
 grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.0-1)" .xgc2/scripts/package_debs.sh
 
 grep -q '<xgc2_math/filter/butterworth_filter.hpp>' gazebo_sim_vrpn_bridge/src/gazebo_vrpn_server_node.cpp
