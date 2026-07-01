5ee49eae6f68b09cf7663ed99893c5e8934a9272
Lxk_buaa <867768510@qq.com>
2026-07-01 21:53:18 +0800

    Bump Gazebo sim tools apt version

--- .xgc2/scripts/check_package_compliance.sh
+++ .xgc2/scripts/check_package_compliance.sh
@@ -83,10 +83,10 @@ if search_files 'ros-noetic-xgc2-controller' .github .xgc2 gazebo_sim_examples g
   exit 1
 fi
 
-grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.8-1)" .xgc2/product.yml
-grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.0-1)" .xgc2/product.yml
-grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.8-1)" .xgc2/scripts/package_debs.sh
-grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.0-1)" .xgc2/scripts/package_debs.sh
+grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.13-1)" .xgc2/product.yml
+grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.1-1)" .xgc2/product.yml
+grep -q "ros-noetic-xgc2-multirotor-controller (>= 1.0.13-1)" .xgc2/scripts/package_debs.sh
+grep -q "ros-noetic-xgc2-ugv-controller (>= 1.0.1-1)" .xgc2/scripts/package_debs.sh
 
 grep -q '<xgc2_math/filter/butterworth_filter.hpp>' gazebo_sim_vrpn_bridge/src/gazebo_vrpn_server_node.cpp
 grep -q 'xgc2_math::SecondOrderButterworthLowPass' gazebo_sim_vrpn_bridge/src/gazebo_vrpn_server_node.cpp
